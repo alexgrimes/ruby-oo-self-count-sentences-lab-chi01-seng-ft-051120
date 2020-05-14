8215400c25c572a4cf45598dac46b29125bec245
Alex Grimes <alex@alexgrimes.com>
2020-05-14 15:21:52 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,35 @@
 require 'pry'
 
 class String
+  attr_accessor :string
 
   def sentence?
-    
+    if self.end_with?('.')
+      true 
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?('?')
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    array = self.split(/[.!?]/).reject {|string| string.empty?}
+    p array.count
+   # binding.pry
   end
 end
